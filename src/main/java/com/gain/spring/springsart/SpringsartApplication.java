f43660a95d0e4fb2f09eb9da517319cd8501f27d
Gain Lee <heli1420@naver.com>
2025-04-10 16:20:59 +0900

    initial commit

---
+++ src/main/java/com/gain/spring/springsart/SpringsartApplication.java
@@ -0,0 +1,13 @@
+package com.gain.spring.springsart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsartApplication.class, args);
+	}
+
+}
