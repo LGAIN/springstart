e5c893abfe77a9aa7407234b77eeca2c46327605
Gain Lee <heli1420@naver.com>
2025-04-11 14:15:26 +0900

    [#12] UserEntity 생성, 테이블 자동 생성 확인

---
+++ src/main/java/com/gain/spring/springstart/service/UserEntity.java
@@ -0,0 +1,27 @@
+package com.gain.spring.springstart.service;
+
+import jakarta.persistence.*;
+
+@Entity // user라는 테이블과 매핑
+@Table(name = "users") // 테이블 이름 지정
+public class UserEntity {
+    @Id // 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가 ID
+    private Long id;
+
+    private String name; // column 이름은 name
+    private String email; // column 이름은 email
+    private int age; // column 이름은 age
+
+    // 기본 생성자
+    public UserEntity() {
+
+    }
+
+    public UserEntity(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+}
